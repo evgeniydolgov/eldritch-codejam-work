82599802aad56290d257e34fd79cd12a82be4555
evgeniydolgov <amyr214519@gmail.com>
2022-08-26 12:42:39 +0300

    init: created eldritch-codejam task

---
+++ data/mythicCards/blue/index.js
@@ -0,0 +1,76 @@
+const cardsDataBlue = [
+  {
+    id: 'blue1',
+    cardFace: './assets/MythicCards/blue1.png',
+    difficulty: 'hard',
+    color:'blue'
+  },
+  {
+    id: 'blue2',
+    cardFace: './assets/MythicCards/blue2.png',
+    difficulty: 'hard',
+    color:'blue'
+  },
+  {
+    id: 'blue3',
+    cardFace: './assets/MythicCards/blue3.png',
+    difficulty: 'easy',
+    color:'blue'
+  },
+  {
+    id: 'blue4',
+    cardFace: './assets/MythicCards/blue4.png',
+    difficulty: 'easy',
+    color:'blue'
+  },
+  {
+    id: 'blue5',
+    cardFace: './assets/MythicCards/blue5.png',
+    difficulty: 'easy',
+    color:'blue'
+  },
+  {
+    id: 'blue6',
+    cardFace: './assets/MythicCards/blue6.png',
+    difficulty: 'hard',
+    color:'blue'
+  },
+  {
+    id: 'blue7',
+    cardFace: './assets/MythicCards/blue7.png',
+    difficulty: 'normal',
+    color:'blue'
+  },
+  {
+    id: 'blue8',
+    cardFace: './assets/MythicCards/blue8.png',
+    difficulty: 'hard',
+    color:'blue'
+  },
+  {
+    id: 'blue9',
+    cardFace: './assets/MythicCards/blue9.png',
+    difficulty: 'normal',
+    color:'blue'
+  },
+  {
+    id: 'blue10',
+    cardFace: './assets/MythicCards/blue10.png',
+    difficulty: 'easy',
+    color:'blue'
+  },
+  {
+    id: 'blue11',
+    cardFace: './assets/MythicCards/blue11.png',
+    difficulty: 'normal',
+    color:'blue'
+  },
+  {
+    id: 'blue12',
+    cardFace: './assets/MythicCards/blue12.png',
+    difficulty: 'normal',
+    color:'blue'
+  },
+]
+
+export default cardsDataBlue
